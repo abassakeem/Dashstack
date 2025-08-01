2810b83a565b3f4f12495fa1718093f1b1b3545d
abassakeem <lanreabassab1@gmail.com>
2025-08-01 13:26:15 +0100

    login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className="main-bg bg-cover bg-center h-screen font-sans text-center p-4  text-primary">
+      Login Page
+      {/* Add your login form or components here */}
+    </div>
+  )
+}
