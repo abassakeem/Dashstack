44d7de7c454eed16f5b2cf608ed48b05d874cdf3
abassakeem <lanreabassab1@gmail.com>
2025-08-02 14:08:09 +0100

    dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,30 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { AppSidebar } from "@/components/app-sidebar"
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { DataTable } from "@/components/data-table"
+import { SectionCards } from "@/components/section-cards"
+import { SiteHeader } from "@/components/site-header"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+import data from "./data.json"
+
+export default function Page({children} : {children: React.ReactNode}) {
   return (
-    <div>
-      <header>
-        <h1>Dashboard layout</h1>
-      </header>
-      <main>{children}</main>
-    </div>
-  );
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+       {children}
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
